50762bab3a7a5fe0fa65d138e57c84c2792f76cc
RambabooPrajapati <145271685+RambabooPrajapati@users.noreply.github.com>
2023-12-27 07:02:46 +0000

    variable scope

--- JSBasics/index.js
+++ JSBasics/index.js
@@ -1,1 +1,5 @@
-console.log("Hi, Ram")
+// console.log("Hi, Ram");
+
+const name= "Rambabu Prajapati" // String type variable contain block level scope and can't change its value.
+let surname = "Prajapati" // String type variable contain block level scope
+
